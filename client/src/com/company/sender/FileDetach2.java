93d360d3c0e3a1be5d91dc0456d171319f8ef5a3
Lam Dao <38157237+Lamdn8@users.noreply.github.com>
2019-01-04 15:10:04 +0700

    Add files via upload

--- client/src/com/company/sender/FileDetach2.java
+++ client/src/com/company/sender/FileDetach2.java
@@ -13,7 +13,7 @@ public class FileDetach2 {
      *           -->ex: FileDetach2.java000000000.txt
      *           -->ex: FileDetach2.java000000001.txt
      */
-    private int partSize = 80; //partSize = 10MB = 80000000
+    private int partSize = 80000; //partSize = 10MB = 80000000
 //    private boolean folder;
     private File[] listOfFile;
     private FileInputStream fileInputStream;
@@ -21,20 +21,23 @@ public class FileDetach2 {
 
     public FileDetach2(String fileAddr){
         try {
-            boolean folder = new File(fileAddr).mkdirs();
             File file = new File(fileAddr);          //get file from file address   ex: ./src/com/company/main.java
             String fileParent = file.getParent();    //get file parent name string  ex: ./src/com/company
             String fileName = file.getName();        //get file name string         ex: main.java
-            fileInputStream = new FileInputStream(file);
+//            fileInputStream = new FileInputStream(file);
+            RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+            new File(fileAddr+"0").mkdirs();
 
-            int partNumber = (int) Math.ceil(file.length()/partSize);    //round up ex: Math.ceil(1.01) = 2
+
+            int partNumber = (int) Math.ceil((float)file.length()/partSize);    //round up ex: Math.ceil(1.01) = 2
             listOfFile = new File[partNumber];
 
             //detach file binary to separate .txt files
             for (int i = 0; i < partNumber-1; i++) {
                 //set name to all part
                 String indexName = String.format("%8s", Integer.toString(i)).replace(' ', '0');
-                listOfFile[i] = new File(fileParent + "/" + fileName + indexName + ".txt");
+//                listOfFile[i] = new File(fileParent + "/" + fileName + "/" + fileName + indexName + ".txt");
+                listOfFile[i] = new File(fileAddr+"0/" + fileName + indexName);
 
                 //Detach
                 FileOutputStream fileOutputStream = new FileOutputStream(listOfFile[i].getParentFile() +
@@ -42,17 +45,31 @@ public class FileDetach2 {
 
                 byte[] bytesread = new byte[partSize];
 
-                fileInputStream.read(bytesread, partSize*i, partSize);
+                randomAccessFile.seek(i*partSize);
+                randomAccessFile.read(bytesread, 0, partSize);
                 fileOutputStream.write(bytesread);
+
                 fileOutputStream.close();
             }
 
             //detach last part - because last part's size could be lest than partSize
+            int i = partNumber - 1;
             int lastPartLength = (int) file.length() - (partNumber-1)*partSize;
             byte[] lastbytes = new byte[lastPartLength];
-            fileInputStream.read(lastbytes, partSize*(partNumber-1), lastPartLength);
-            FileOutputStream fileOutputStream = new FileOutputStream(listOfFile[partNumber-1].getParentFile() +
-                    "/"+ listOfFile[partNumber-1].getName());
+            String indexName = String.format("%8s", Integer.toString(i)).replace(' ', '0');
+//            listOfFile[i] = new File(fileParent + "/" + fileName + "/" + fileName + indexName + ".txt");
+            listOfFile[i] = new File(fileAddr+"0/" + fileName + indexName);
+
+            //Detach
+            FileOutputStream fileOutputStream = new FileOutputStream(listOfFile[i].getParentFile() +
+                    "/"+ listOfFile[i].getName());
+
+            randomAccessFile.seek(i*partSize);
+            randomAccessFile.read(lastbytes, 0, lastPartLength);
+            fileOutputStream.write(lastbytes);
+
+            fileOutputStream.close();
+            randomAccessFile.close();
 
         } catch (IOException e) {
             e.printStackTrace();
